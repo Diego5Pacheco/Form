7bf24cce9c9edf2541f12853423cd0152728b9b0
Diego Silva <125151823+Diego5Pacheco@users.noreply.github.com>
2024-08-31 16:18:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Função para exibir mensagem quando o botão Enviar é clicado
+document.getElementById('submitBtn').addEventListener('click', function () {
+    var msg = "Infelizmente, o botão de envio ainda não está operacional.";
+    var msgElement = document.getElementById('msg');
+    msgElement.textContent = msg;
+    msgElement.style.display = 'block';
+});
+
+// Função para limpar o formulário quando o botão Limpar é clicado
+document.getElementById('clearBtn').addEventListener('click', function () {
+    document.getElementById('formulario').reset();
+});
